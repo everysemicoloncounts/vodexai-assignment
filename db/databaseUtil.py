85fff93a90257868bc72806b1189adb9d1e15192
thehumblecodergp <gouravpratap.202cd006@nitk.edu.in>
2025-02-20 00:49:48 +0530

    refactoring

--- db/databaseUtil.py
+++ db/databaseUtil.py
@@ -7,23 +7,22 @@ from app.constants.constants import db_name, item_collection_name
 load_dotenv()
 
 def createMongoUri():
-    uriString = ""
-    uriString += "mongodb+srv://" + os.getenv("MONGO_USER_NAME") + ":" + os.getenv("MONGO_USER_PASSWORD") + "@" + os.getenv("MONGO_CLUSTER_NAME") + ".l8vsp.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPractice"
-    return uriString
-def connect_to_mongo():
-    mongo_url = createMongoUri() if os.getenv("IS_DB_HOSTED") == "TRUE" else os.getenv("LOCAL_MONGO_URI")
+    uriString = "mongodb+srv://" + os.getenv("MONGO_USER_NAME") + ":" + os.getenv("MONGO_USER_PASSWORD") + "@" + os.getenv("MONGO_CLUSTER_NAME") + ".l8vsp.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPractice" if os.getenv("IS_DB_HOSTED") == "TRUE" else os.getenv("LOCAL_MONGO_URI")
+    return uriString if os.getenv("IS_DB_HOSTED") == "TRUE" else os.getenv("LOCAL_MONGO_URI")
+
+def get_mongo_client():
+    mongo_url = createMongoUri()
     client = MongoClient(mongo_url, server_api=ServerApi('1'))
-    # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
-        return client[db_name]
+        return client
     except Exception as e:
         print(e)
 
 def get_database():
-    db = connect_to_mongo()
-    return db
+    client = get_mongo_client()
+    return client[db_name]
 
 def get_Item_Collection():
     db = get_database()
