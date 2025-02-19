b3d5da718be433c1dce7596dff60366266a917d3
thehumblecodergp <gouravpratap.202cd006@nitk.edu.in>
2025-02-20 00:44:53 +0530

    added the Mongo Atlas code, now pulling data from deployed Mongo Server

--- db/databaseUtil.py
+++ db/databaseUtil.py
@@ -1,32 +1,28 @@
 import os
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 from app.constants.constants import db_name, item_collection_name
 
 load_dotenv()
 
-client = None
-db = None
-
 def createMongoUri():
     uriString = ""
-    uriString += "mongodb+srv://" + os.getenv("MONGO_USER_NAME") + ":" + os.getenv("MONGO_USER_PASSWORD") + "@" + os.getenv("MONGO_CLUSTER_NAME") + ".l8vsp.mongodb.net/"
+    uriString += "mongodb+srv://" + os.getenv("MONGO_USER_NAME") + ":" + os.getenv("MONGO_USER_PASSWORD") + "@" + os.getenv("MONGO_CLUSTER_NAME") + ".l8vsp.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPractice"
     return uriString
-
 def connect_to_mongo():
-    global client, db
     mongo_url = createMongoUri() if os.getenv("IS_DB_HOSTED") == "TRUE" else os.getenv("LOCAL_MONGO_URI")
-    print("mongo_url", mongo_url)
+    client = MongoClient(mongo_url, server_api=ServerApi('1'))
+    # Send a ping to confirm a successful connection
     try:
-        client = MongoClient(mongo_url)
-        db = client[db_name]
-        print("Connected to MongoDB!")
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        return client[db_name]
     except Exception as e:
-        print("pyMongo connection error")
+        print(e)
 
 def get_database():
-    connect_to_mongo()
-    global db
+    db = connect_to_mongo()
     return db
 
 def get_Item_Collection():
